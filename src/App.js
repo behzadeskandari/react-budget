328f5d356f93c64d56bc31f8cdf1fbdcaf7eb19c
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2021-11-25 09:15:16 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, ButtonGroup, Container,Form,Grid,GridRow,Header,Icon,Segment,Statistic} from 'semantic-ui-react'
+import MainHeader from './components/MainHeader';
+import ButtonSaveOrCancel from './components/ButtonSaveOrCancel';
+import NewEntryForm from './components/NewEntryForm';
+import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <MainHeader title="budget"></MainHeader>
+      
+      <DisplayBalance title="Your Balance" value="3255.54" size="small"/>
+             
+      <DisplayBalances/>
+
+      <MainHeader title="History" type="h3">History</MainHeader>
+      
+      <EntryLine description="Income" value="$10.00"  color="red"/>
+      
+      <EntryLine description="Expense" value="$10.00" isExpense color="red"/>
+
+      <MainHeader as="h3" title="Add new Transaction"></MainHeader>
+      
+      <NewEntryForm/>
+
+    </Container>
   );
 }
 
